41b6f50b737bff307df0dd902624b8f2ed22962b
SongkyoLee <songkyo.lee@cern.ch>
2015-05-04 17:00:03 +0900

    data vs mc RECO pointted at mean pT

---
+++ 2015/001_Acceptance_weight_8rap9pt/fitRatio_kyo01.C
@@ -0,0 +1,224 @@
+// latest version : 2015/02/20
+// Yeonju Go 
+// The code is based on FitAndMakeRatioToy_pt.C by Yeonju.
+// The code generate gaussian mean & sigma of fine bins for the each rapidity bins
+// 2D differential ratio(DATA/MC)
+
+#include <iostream>
+#include <iomanip>
+#include <TF1.h>
+#include <TH1.h>
+#include <TFile.h>
+#include <TTree.h>
+#include <TCanvas.h>
+#include <TLegend.h>
+#include <TPaveText.h>
+#include <TLatex.h>
+#include <TRandom.h>
+#include "TFitResult.h"
+#include <ctime>
+#include <string>
+#include <sstream>
+#include <TGraph.h>
+#include <TGraphErrors.h>
+#include <TGraphAsymmErrors.h>
+
+//#include "KYOcommonOptMulti.h"
+#include "KYOcommonOpt.h"
+
+//#include "../HiForestAnalysis/hiForest.h"
+//#include "../gammaJetAnalysis/CutAndBinCollection2012.h"
+
+Double_t ptArrNum[] = {0.0, 3.0, 4.0, 5.0, 6.5, 7.5, 8.5, 10., 14., 30.};
+const Int_t nPt = sizeof(ptArrNum)/sizeof(Double_t)-1;
+
+Double_t ptFineArrNum[] = {0.0, 0.6, 1.2, 1.8, 2.4, 3.0, 3.2, 3.4, 3.6, 3.8, 4.0, 4.2, 4.4, 4.6, 4.8, 5.0, 5.3, 5.6, 5.9, 6.2, 6.5, 6.7, 6.9, 7.1, 7.3, 7.5, 7.7, 7.9, 8.1, 8.3, 8.5, 8.8, 9.1, 9.4, 9.7, 10., 10.8, 11.6, 12.4, 13.2, 14., 17.2, 20.4, 23.6, 26.8, 30.};
+const Int_t nPtFine = sizeof(ptFineArrNum)/sizeof(Double_t)-1;
+
+Double_t rapArrNumFB[] = {1.93, 1.5, 0.9, 0., -0.9, -1.5, -1.93, -2.4, -2.87};// for pt dist.
+//Double_t rapArrNumBF[] = {-2.87, -2.4, -1.93, -1.5, -0.9, 0., 0.9, 1.5, 1.93};// for rap dist.
+const Int_t nRap = sizeof(rapArrNumFB)/sizeof(Double_t)-1;
+
+double fitHevi(double *x, double *par);
+double fitExp(double *x, double *par);
+void formRapArr(Double_t binmin, Double_t binmax, string* arr);
+
+void fitRatio_kyo01(bool isPrompt=true, bool isPbp=true, char* dirName="8rap9pt"){
+    gRandom->SetSeed(time(0));
+		gROOT->Macro("./JpsiStyle.C");
+
+		//latex box for beam, rapidity, pT info
+		TLatex* latex = new TLatex();
+		latex->SetNDC();
+		latex->SetTextAlign(12);
+		latex->SetTextSize(0.04);
+
+		string rapArr[nRap];
+		for (Int_t iy=0; iy<nRap; iy++) {
+			formRapArr(rapArrNumFB[iy+1], rapArrNumFB[iy], &rapArr[iy]);
+			cout << iy <<"th rapArr = " << rapArr[iy] << endl;
+		}
+
+
+//    TFile* fin = new TFile(Form("../pPbJPsiAnalysis/2015/004_closure/DataMcReco_8rap9pt/DataMcRecoPt_isPropmt%d.root",(int)isPrompt));//in KNU 
+    TFile* fin = new TFile(Form("../004_closure/DataMcReco_%s/DataMcRecoPt_isPropmt%d.root",dirName,(int)isPrompt));//in KNU  //KYO
+    string runstring;
+    string runstringOverlay;
+    if(isPbp) {runstring="Pbp"; runstringOverlay="pPb";}
+    else {runstring="pPb"; runstringOverlay="Pbp"; }
+
+    TGraphAsymmErrors* gRatio[nRap];
+    TGraphAsymmErrors* gRatioOverlay[nRap];
+    for(int iy=0;iy<nRap;iy++){
+        gRatio[iy]=(TGraphAsymmErrors*)fin->Get(Form("gRatio_%s_%d",runstring.c_str(),iy));
+        gRatioOverlay[iy]=(TGraphAsymmErrors*)fin->Get(Form("gRatio_%s_%d",runstringOverlay.c_str(),iy));
+				gRatio[iy]->SetName(Form("gRatio_%d",iy));
+				gRatioOverlay[iy]->SetName(Form("gRatioOverlay_%d",iy));
+    }
+
+    //TFile* fout= new TFile(Form("ToyGaussian_isPrompt%d_%s.root",(int)isPrompt,runstring.c_str()),"recreate");
+    //TFile* fout= new TFile(Form("ToyGaussian_isPrompt%d_%s_kyo01.root",(int)isPrompt,runstring.c_str()),"recreate");//KYO
+    TFile* fout= new TFile(Form("fitRatio_isPrompt%d_%s_kyo01.root",(int)isPrompt,runstring.c_str()),"recreate");//KYO
+
+    ////////////////////////////////////////////////////////////
+    // Output gaussian parameter histograms
+
+    TH1D* hWeight[nRap];
+    // TH1D* hWeightSigma[nRap];
+    for(int iy=0;iy<nRap;iy++){
+//				if (iy!=0) continue ; //KYO
+        hWeight[iy]=new TH1D(Form("hWeight_%s_%d",runstring.c_str(),iy), Form("hWeight_%s_%d",runstring.c_str(),iy), nPtFine, ptFineArrNum);
+    //    hWeightSigma[iy]=new TH1D(Form("hWeightSigma_%s_%d",runstring.c_str(),iy), Form("hWeightSigma_%s_%d",runstring.c_str(),iy), nPtFine, ptFineArrNum);
+    }
+#if 0
+    int rapiditybin[nRap],nfpt[nRap];
+    double mean[nRap][nPtFine],sigma[nRap][nPtFine];
+
+    TTree* toyGaus[nRap];
+    for(int iy=0;iy<nRap;iy++){
+        toyGaus[iy]=new TTree(Form("toyGaus%d",iy),Form("rapidity bin %dth",iy));
+        toyGaus[iy]->Branch("rapiditybin", &rapiditybin[iy]);
+        toyGaus[iy]->Branch("nfpt", &nfpt[iy]);
+        toyGaus[iy]->Branch("mean", mean[iy],"mean[nfpt]/D");
+        toyGaus[iy]->Branch("sigma", sigma[iy],"sigma[nfpt]/D");
+    }
+#endif
+
+    ////////////////////////////////////////////////////////////
+    // Fitting
+
+//    TF1* fhevi= new TF1("fhevi","[0]/(exp((-x+[1])/[2] +1)+[3])+[4]",0,30);
+//    fhevi->SetParameters(-4.795, 1.653, 0.923, 3.018, 2.369);
+//    TF1* fexp= new TF1("fexp","[0]/(1+exp([1]*x))+[2]/x",0.0,30.0);//Prompt
+    TF1* func[nRap];
+   
+//    TCanvas* c1 = new TCanvas("c1", "", 1200,1500);
+//    c1 -> Divide(3,3);
+    TCanvas* c1 = new TCanvas("c1", "", 1600,800); //KYO
+    c1 -> Divide(4,2); // KYO
+    for(int iy=0;iy<nRap;iy++){
+        c1->cd(iy+1);
+        if(iy==0 || iy==nRap-1){
+            func[iy] = new TF1(Form("func_%d",iy), fitHevi,0.0,30.0,5);
+            func[iy]->SetParameters(-4.795, 1.653, 0.923, 3.018, 2.369);
+            if(isPrompt==1 && isPbp==0 && iy==0) { 
+          //      func[iy]->SetParameters(-51.7167, -0.694876, 0.491544,4.55470,12.1672);
+#if 1
+                func[iy]->FixParameter(0,-5.17167e+01 );
+                func[iy]->FixParameter(1,-6.94876e-01 );
+                //func[iy]->FixParameter(2,4.91544e-01 );
+                func[iy]->FixParameter(2,9.91544e-01 );
+                //func[iy]->FixParameter(3,4.55470e+00 );
+                //func[iy]->FixParameter(3,3.55470e+00 );
+                //func[iy]->FixParameter(4,1.11672e+01 );
+#endif
+            }
+        } else if(isPrompt==1 && isPbp==0 && iy==4){
+                func[iy] = new TF1(Form("func_%d",iy), fitHevi,0.0,30.0,5); 
+                func[iy]->SetParameters(-3.40747e+02,3.32373e+00,6.51294e-01,2.98861e+00,1.14947e+02);
+        } else{
+                func[iy] = new TF1(Form("func_%d",iy), fitHevi,0.0,30.0,5); 
+                func[iy]->SetParameters(-3.40747e+02,3.32373e+00,6.51294e-01,2.98861e+00,1.14947e+02);
+            		//KYO
+								if (iy==1 || iy==6) func[iy]->SetParameters(-4.795, 1.653, 0.923, 3.018, 2.369);
+								else if (isPrompt && iy==2) {
+         					func[iy]->FixParameter(0,-3.40743e+02);
+          				func[iy]->FixParameter(1,-1.37032e-01);
+          				func[iy]->FixParameter(2,1.19902);
+								}									 
+/*
+            func[iy] = new TF1(Form("func_%d",iy), fitExp,0.0,30.0,3);
+            if(isPrompt==0 && isPbp==1 && (iy==2 || iy==3 || iy==4 || iy==5) ) func[iy]->SetParameters(-1.01556e+01,1.51712e-01,2.80282e+01);
+            if(isPrompt==0 && isPbp==0 && (iy==2 || iy==3 || iy==4) ) func[iy]->SetParameters(-1.01556e+01,1.51712e-01,2.80282e+01);
+            if(isPrompt==1 && isPbp==0 && (iy==3) ) func[iy]->SetParameters(-1.01556e+01,1.51712e-01,2.80282e+01);
+            if(isPrompt==1 && isPbp==1 && (iy==3) ) func[iy]->SetParameters(-1.01556e+01,1.51712e-01,2.80282e+01);
+*/
+        }
+        //KYO for TGraphAsymmErrors only
+				if (!isPrompt && iy==0){
+        	func[iy]->SetParameters(-1.04829e+01,-5.38813e+00,2.10443e+00,1.74460e+00,6.89848e+02);
+          func[iy]->FixParameter(0,-1.04829e+01);
+          func[iy]->FixParameter(1,-5.38813e+00);
+          func[iy]->FixParameter(2,2.10443e+00);
+				}
+				SetGraphStyle(gRatio[iy],2,0);
+				SetGraphStyle(gRatioOverlay[iy],4,10);
+				func[iy]->SetLineColor(kRed);
+				gRatio[iy]->Fit(Form("func_%d",iy));
+        gRatio[iy]->Draw("AP");
+        gRatioOverlay[iy]->Draw("P");
+				//func[iy]->Draw("same");
+				if (iy==0){
+					if (isPrompt) latex->DrawLatex(0.23,0.23,"Prompt J/#psi Pbp");
+					else latex->DrawLatex(0.23, 0.23,"Non-prompt J/#psi Pbp");
+				}
+				latex->DrawLatex(0.56,0.80,Form("%s",rapArr[iy].c_str()));
+				dashedLine(0.,1.,25.,1.,1,1);
+
+        /*
+				for(int ifpt=0;ifpt<nPtFine;ifpt++){
+            int hBin = gRatio[iy]->FindBin(ptFineArrNum[ifpt+1]-0.1);
+            double mean = func[iy]->Eval(ptFineArrNum[ifpt+1]);
+            double sigma = gRatio[iy]->GetBinError(hBin) / mean;
+            hWeight[iy]->SetBinContent(ifpt+1, mean);
+            hWeight[iy]->SetBinError(ifpt+1, sigma);
+            //cout <<"x : "<<ptFineArrNum[ifpt+1]-0.01<< ", gRatio bin : " <<hBin << ", Ratio error value : " <<  gRatio[iy]->GetBinError(hBin) << endl;
+            //cout << iy+1 << "th rapidity, "<< ifpt<<"th fine ptbin, mean = "<< mean <<", sigma = "<<sigma<<endl;
+        }
+				*/
+    }
+    c1 -> Update();
+
+    fout->cd();	
+    for(int iy=0;iy<nRap;iy++){
+        //hWeight[iy]->Write();
+        gRatio[iy]->Write();
+        func[iy]->Write();
+    }
+    c1->Write();
+    fout->Close();
+
+}//end of main func.
+
+double fitHevi(double *x, double *par) {
+    return par[0]/(exp((-x[0]+par[1])/par[2] +1)+par[3])+par[4];
+}
+double fitExp(double *x, double *par) {
+    return par[0]/(1+exp(par[1]*x[0]))+par[2]/x[0];
+}
+
+void formRapArr(Double_t binmin, Double_t binmax, string* arr) {
+	Double_t intMin, intMax; 
+	Double_t fracMin = modf(binmin, &intMin);
+	Double_t fracMax = modf(binmax, &intMax);
+	if ( fracMin == 0 && fracMax == 0 ) {
+		*arr = Form("%.0f < y_{CM} < %.0f", binmin, binmax);
+	} else if ( fracMin != 0 && fracMax == 0 ) {
+		*arr = Form("%.2f < y_{CM} < %.0f", binmin, binmax);
+	} else if ( fracMin == 0 && fracMax != 0 ) {
+		*arr = Form("%.0f < y_{CM} < %.2f", binmin, binmax);
+	} else {
+		*arr = Form("%.2f < y_{CM} < %.2f", binmin, binmax);
+	}
+}
+
--- 2015/004_closure/draw_1D_data_mc_reco_pt.C
+++ 2015/004_closure/draw_1D_data_mc_reco_pt.C
@@ -3,6 +3,8 @@
 #include <TFile.h>
 #include <TTree.h>
 #include <TGraph.h>
+#include <TGraphErrors.h>
+#include <TGraphAsymmErrors.h>
 #include <vector>
 #include <TSystem.h>
 #include <TCanvas.h>
@@ -30,6 +32,27 @@ void formPtArr(Double_t binmin, Double_t binmax, string* arr);
 void draw_1D_data_mc_reco_pt(char* dirName = "8rap9pt", bool isPrompt=false, bool isLog=false)
 {
 	gROOT->Macro("./JpsiStyle.C");
+
+	const int ntmp = 9;
+	Double_t px[8][ntmp]; //x point
+	Double_t pxtmp[8][ntmp]; //x point
+	Double_t pytmp[8][ntmp]; //y point
+	Double_t ex[ntmp]; // x error
+	
+	px[0] = {1.80888, 3.48529, 4.47275, 5.68792, 6.9644, 7.96271, 9.1754, 11.5315, 17.7588}; 
+	px[1] = {0.00000, 3.53123, 4.5027, 5.71709, 6.96523, 7.9693, 9.17314, 11.4952, 17.6927}; 
+	px[2] = {0.00000, 0.00000, 0.00000, 0.00000, 7.01977, 7.99712, 9.19936, 11.5743, 17.7732}; 
+	px[3] = {0.00000, 0.00000, 0.00000, 0.00000, 7.12292, 8.01305, 9.22816, 11.6279, 17.8879}; 
+	px[4] = {0.00000, 0.00000, 0.00000, 0.00000, 7.05476, 8.00208, 9.21589, 11.5645, 17.7176}; 
+	px[5] = {0.00000, 0.00000, 0.00000, 5.82095, 6.97886, 7.96732, 9.18979, 11.5158, 17.4116}; 
+	px[6] = {0.00000, 3.52893, 4.48328, 5.69351, 6.96188, 7.95707, 9.14886, 11.4747, 17.231}; 
+	px[7] = {1.78552, 3.47853, 4.46938, 5.6761, 6.96419, 7.97702, 9.16158, 11.5077, 17.3061}; 
+	ex = {0,0,0,0,0,0,0,0,0};
+
+
+
+
+
 	
 	//rap array in yCM (from forward to backward)
 	Double_t rapArrNumFB[] = {1.93, 1.5, 0.9, 0., -0.9, -1.5, -1.93, -2.4, -2.87};// for pt dist.
@@ -223,7 +246,7 @@ void draw_1D_data_mc_reco_pt(char* dirName = "8rap9pt", bool isPrompt=false, boo
 	latex->SetNDC();
 	latex->SetTextAlign(12);
 	latex->SetTextSize(0.04);
-
+	
 	// --- Pbp (1st run)
 	TCanvas* c_Pbp = new TCanvas("c_Pbp","c_Pbp",200,10,1600,800);
 	c_Pbp->Divide(4,2);
@@ -232,7 +255,7 @@ void draw_1D_data_mc_reco_pt(char* dirName = "8rap9pt", bool isPrompt=false, boo
 	for (Int_t iy = 0; iy < nbinsX; iy++) {
 		c_Pbp->cd(iy+1);
 		SetHistStyle(h1D_data_Pbp[iy],1,0);
-		SetHistStyle(h1D_MC_Pbp[iy],2,0);
+		SetHistStyle(h1D_MC_Pbp[iy],2,10);
 		h1D_data_Pbp[iy]->GetXaxis()->SetTitle("p_{T} (GeV/c)");
 		h1D_data_Pbp[iy]->GetXaxis()->CenterTitle();
 		h1D_data_Pbp[iy]->GetXaxis()->SetRangeUser(0.,30.);
@@ -264,7 +287,7 @@ void draw_1D_data_mc_reco_pt(char* dirName = "8rap9pt", bool isPrompt=false, boo
 		if (isLog) gPad->SetLogy(1);
 		else gPad->SetLogy(0);
 		SetHistStyle(h1D_data_pPb[iy],1,0);
-		SetHistStyle(h1D_MC_pPb[iy],2,0);
+		SetHistStyle(h1D_MC_pPb[iy],2,10);
 		h1D_data_pPb[iy]->GetXaxis()->SetTitle("p_{T} (GeV/c)");
 		h1D_data_pPb[iy]->GetXaxis()->CenterTitle();
 		h1D_data_pPb[iy]->GetXaxis()->SetRangeUser(0.,30.);
@@ -287,7 +310,7 @@ void draw_1D_data_mc_reco_pt(char* dirName = "8rap9pt", bool isPrompt=false, boo
 	}
 	c_pPb->SaveAs(Form("DataMcReco_%s/DataMcRecoPt_pPb_isPrompt%d_isLog%d.pdf",dirName,(int)isPrompt,(int)isLog));
 	legUR->Clear();
-
+	
 	// --- tot (1st+2nd)
 	TCanvas* c_tot = new TCanvas("c_tot","c_tot",200,10,1600,800);
 	c_tot->Divide(4,2);
@@ -296,7 +319,7 @@ void draw_1D_data_mc_reco_pt(char* dirName = "8rap9pt", bool isPrompt=false, boo
 		if (isLog) gPad->SetLogy(1);
 		else gPad->SetLogy(0);
 		SetHistStyle(h1D_data_tot[iy],1,0);
-		SetHistStyle(h1D_MC_tot[iy],2,0);
+		SetHistStyle(h1D_MC_tot[iy],2,10);
 		h1D_data_tot[iy]->GetXaxis()->SetTitle("p_{T} (GeV/c)");
 		h1D_data_tot[iy]->GetXaxis()->CenterTitle();
 		h1D_data_tot[iy]->GetXaxis()->SetRangeUser(0.,30.);
@@ -351,7 +374,7 @@ void draw_1D_data_mc_reco_pt(char* dirName = "8rap9pt", bool isPrompt=false, boo
 		else latex->DrawLatex(0.56,0.88,Form("%s",rapArr[iy].c_str()));
 		dashedLine(0.,1.,30.,1.,1,1);
 	}
-	c_Pbp->SaveAs(Form("DataMcReco_%s/DataMcRecoPtRatio_Pbp_isPrompt%d_isLog%d.pdf",dirName,(int)isPrompt,(int)isLog));
+//	c_Pbp->SaveAs(Form("DataMcReco_%s/DataMcRecoPtRatio_Pbp_isPrompt%d_isLog%d.pdf",dirName,(int)isPrompt,(int)isLog));
 	legBL->Clear();
 
 	// --- pPb (2nd run)
@@ -376,9 +399,9 @@ void draw_1D_data_mc_reco_pt(char* dirName = "8rap9pt", bool isPrompt=false, boo
 		else latex->DrawLatex(0.56,0.88,Form("%s",rapArr[iy].c_str()));
 		dashedLine(0.,1.,30.,1.,1,1);
 	}
-	c_pPb->SaveAs(Form("DataMcReco_%s/DataMcRecoPtRatio_pPb_isPrompt%d_isLog%d.pdf",dirName,(int)isPrompt,(int)isLog));
+//	c_pPb->SaveAs(Form("DataMcReco_%s/DataMcRecoPtRatio_pPb_isPrompt%d_isLog%d.pdf",dirName,(int)isPrompt,(int)isLog));
 	legBL->Clear();
-
+	
 	// --- tot (1st + 2nd)
 	for (Int_t iy = 0; iy < nbinsX; iy++) {
 		c_tot->cd(iy+1);
@@ -401,8 +424,112 @@ void draw_1D_data_mc_reco_pt(char* dirName = "8rap9pt", bool isPrompt=false, boo
 		else latex->DrawLatex(0.56,0.88,Form("%s",rapArr[iy].c_str()));
 		dashedLine(0.,1.,30.,1.,1,1);
 	}
-	c_tot->SaveAs(Form("DataMcReco_%s/DataMcRecoPtRatio_tot_isPrompt%d_isLog%d.pdf",dirName,(int)isPrompt,(int)isLog));
+//	c_tot->SaveAs(Form("DataMcReco_%s/DataMcRecoPtRatio_tot_isPrompt%d_isLog%d.pdf",dirName,(int)isPrompt,(int)isLog));
 	legBL->Clear();
+
+	c_Pbp->Clear();
+	c_pPb->Clear();
+	c_tot->Clear();
+
+	//////////////////////////////////////////////////////////////////
+	// convert to TGraphAsymErrors only for ratio
+	TGraphAsymmErrors* gRatio_Pbp[nbinsX];
+	TGraphAsymmErrors* gRatio_pPb[nbinsX];
+	TGraphAsymmErrors* gRatio_tot[nbinsX];
+	
+	TCanvas* c_Pbp2 = new TCanvas("c_Pbp2","c_Pbp2",200,10,1600,800);
+	c_Pbp2->Divide(4,2);	
+	TCanvas* c_pPb2 = new TCanvas("c_pPb2","c_pPb2",200,10,1600,800);
+	c_pPb2->Divide(4,2);	
+	TCanvas* c_tot2 = new TCanvas("c_tot2","c_tot2",200,10,1600,800);
+	c_tot2->Divide(4,2);	
+	
+	
+	// --- Pbp (1st run)
+	for (Int_t iy = 0; iy < nbinsX; iy++) {
+		gRatio_Pbp[iy]=new TGraphAsymmErrors(hRatio_Pbp[iy]);
+		gRatio_Pbp[iy]->SetName(Form("gRatio_Pbp_%d",iy));
+		for (Int_t ipt=0; ipt<nbinsY; ipt++ ){
+			gRatio_Pbp[iy]->GetPoint(ipt, pxtmp[iy][ipt], pytmp[iy][ipt]);	
+			gRatio_Pbp[iy]->SetPoint(ipt, px[iy][ipt], pytmp[iy][ipt]);
+			gRatio_Pbp[iy]->SetPointEXlow(ipt, ex[ipt]);
+			gRatio_Pbp[iy]->SetPointEXhigh(ipt, ex[ipt]);
+			cout << "pr : pytmp["<<iy<<"]["<<ipt<<"] = " << pytmp[iy][ipt]<<endl;
+		}
+		c_Pbp2->cd(iy+1);
+		gRatio_Pbp[iy]->GetXaxis()->SetLimits(0.0,25.);
+		gRatio_Pbp[iy]->SetMinimum(0.2);
+		gRatio_Pbp[iy]->SetMaximum(1.8);
+		gRatio_Pbp[iy]->GetXaxis()->SetTitle("p_{T} [GeV/c]");
+		gRatio_Pbp[iy]->GetYaxis()->SetTitle("Ratio");
+		gRatio_Pbp[iy]->Draw("AP");
+		if (iy==0){
+			if (isPrompt) latex->DrawLatex(0.23,0.23,"Prompt J/#psi Pbp");
+			else latex->DrawLatex(0.23, 0.23,"Non-prompt J/#psi Pbp");
+			//latex->DrawLatex(0.56,0.88,Form("%s",rapArr[iy].c_str()));
+		}
+		latex->DrawLatex(0.56,0.88,Form("%s",rapArr[iy].c_str()));
+		dashedLine(0.,1.,25.,1.,1,1);
+	}	
+	c_Pbp2->SaveAs(Form("DataMcReco_%s/DataMcRecoPt_gRatio_Pbp_isPrompt%d.pdf",dirName,(int)isPrompt));
+
+	// --- pPb (2nd run)
+	for (Int_t iy = 0; iy < nbinsX; iy++) {
+		gRatio_pPb[iy]=new TGraphAsymmErrors(hRatio_pPb[iy]);
+		gRatio_pPb[iy]->SetName(Form("gRatio_pPb_%d",iy));
+		for (Int_t ipt=0; ipt<nbinsY; ipt++ ){
+			gRatio_pPb[iy]->GetPoint(ipt, pxtmp[iy][ipt], pytmp[iy][ipt]);	
+			gRatio_pPb[iy]->SetPoint(ipt, px[iy][ipt], pytmp[iy][ipt]);
+			gRatio_pPb[iy]->SetPointEXlow(ipt, ex[ipt]);
+			gRatio_pPb[iy]->SetPointEXhigh(ipt, ex[ipt]);
+			cout << "pr : pytmp["<<iy<<"]["<<ipt<<"] = " << pytmp[iy][ipt]<<endl;
+		}
+		c_pPb2->cd(iy+1);
+		gRatio_pPb[iy]->GetXaxis()->SetLimits(0.0,25.);
+		gRatio_pPb[iy]->SetMinimum(0.2);
+		gRatio_pPb[iy]->SetMaximum(1.8);
+		gRatio_pPb[iy]->GetXaxis()->SetTitle("p_{T} [GeV/c]");
+		gRatio_pPb[iy]->GetYaxis()->SetTitle("Ratio");
+		gRatio_pPb[iy]->Draw("AP");
+		if (iy==0){
+			if (isPrompt) latex->DrawLatex(0.23,0.23,"Prompt J/#psi pPb");
+			else latex->DrawLatex(0.23, 0.23,"Non-prompt J/#psi pPb");
+			//latex->DrawLatex(0.56,0.88,Form("%s",rapArr[iy].c_str()));
+		}
+		latex->DrawLatex(0.56,0.88,Form("%s",rapArr[iy].c_str()));
+		dashedLine(0.,1.,25.,1.,1,1);
+	}	
+	c_pPb2->SaveAs(Form("DataMcReco_%s/DataMcRecoPt_gRatio_pPb_isPrompt%d.pdf",dirName,(int)isPrompt));
+
+
+
+	// --- tot (1st + 2nd)
+	for (Int_t iy = 0; iy < nbinsX; iy++) {
+		gRatio_tot[iy]=new TGraphAsymmErrors(hRatio_tot[iy]);
+		gRatio_tot[iy]->SetName(Form("gRatio_tot_%d",iy));
+		for (Int_t ipt=0; ipt<nbinsY; ipt++ ){
+			gRatio_tot[iy]->GetPoint(ipt, pxtmp[iy][ipt], pytmp[iy][ipt]);	
+			gRatio_tot[iy]->SetPoint(ipt, px[iy][ipt], pytmp[iy][ipt]);
+			gRatio_tot[iy]->SetPointEXlow(ipt, ex[ipt]);
+			gRatio_tot[iy]->SetPointEXhigh(ipt, ex[ipt]);
+			cout << "pr : pytmp["<<iy<<"]["<<ipt<<"] = " << pytmp[iy][ipt]<<endl;
+		}
+		c_tot2->cd(iy+1);
+		gRatio_tot[iy]->GetXaxis()->SetLimits(0.0,25.);
+		gRatio_tot[iy]->SetMinimum(0.2);
+		gRatio_tot[iy]->SetMaximum(1.8);
+		gRatio_tot[iy]->GetXaxis()->SetTitle("p_{T} [GeV/c]");
+		gRatio_tot[iy]->GetYaxis()->SetTitle("Ratio");
+		gRatio_tot[iy]->Draw("AP");
+		if (iy==0){
+			if (isPrompt) latex->DrawLatex(0.23,0.23,"Prompt J/#psi tot");
+			else latex->DrawLatex(0.23, 0.23,"Non-prompt J/#psi tot");
+			//latex->DrawLatex(0.56,0.88,Form("%s",rapArr[iy].c_str()));
+		}
+		latex->DrawLatex(0.56,0.88,Form("%s",rapArr[iy].c_str()));
+		dashedLine(0.,1.,25.,1.,1,1);
+	}	
+	c_tot2->SaveAs(Form("DataMcReco_%s/DataMcRecoPt_gRatio_tot_isPrompt%d.pdf",dirName,(int)isPrompt));
 
 	//////////////////////////////////////////////////////////////////
 	// root file
@@ -412,12 +539,15 @@ void draw_1D_data_mc_reco_pt(char* dirName = "8rap9pt", bool isPrompt=false, boo
 		h1D_data_Pbp[iy]->Write();
 		h1D_MC_Pbp[iy]->Write();
 		hRatio_Pbp[iy]->Write();
+		gRatio_Pbp[iy]->Write();
 		h1D_data_pPb[iy]->Write();
 		h1D_MC_pPb[iy]->Write();
 		hRatio_pPb[iy]->Write();
+		gRatio_pPb[iy]->Write();
 		h1D_data_tot[iy]->Write();
 		h1D_MC_tot[iy]->Write();
 		hRatio_tot[iy]->Write();
+		gRatio_tot[iy]->Write();
 	}
 	fOut->Close();
 	cout << "DataMcRecoPt_isPropmt" <<(int)isPrompt<<".root has been created :)" <<endl; 
