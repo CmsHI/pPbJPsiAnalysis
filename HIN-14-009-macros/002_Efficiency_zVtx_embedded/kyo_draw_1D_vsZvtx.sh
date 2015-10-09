f4a079932c662bbb778e8ce4b9405d60687faaf1
SongkyoLee <songkyo.lee@cern.ch>
2015-10-09 16:50:51 +0200

    new cut

--- HIN-14-009-macros/002_Efficiency_zVtx_embedded/kyo_draw_1D_vsZvtx.sh
+++
@@ -1,5 +0,0 @@
-# no TNP, no zVtx
-root -l -b -q 'draw_1D_vsZvtx.C+(1,1,0,0,0,0,0,"draw_1D_vsZvtx")'
-root -l -b -q 'draw_1D_vsZvtx.C+(1,0,0,0,0,0,0,"draw_1D_vsZvtx")'
-root -l -b -q 'draw_1D_vsZvtx.C+(0,1,0,0,0,0,0,"draw_1D_vsZvtx")'
-root -l -b -q 'draw_1D_vsZvtx.C+(0,0,0,0,0,0,0,"draw_1D_vsZvtx")'
