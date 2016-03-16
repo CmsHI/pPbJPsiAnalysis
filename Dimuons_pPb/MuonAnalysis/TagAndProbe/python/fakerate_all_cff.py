7b7fbf4367d8e85390352fd8c2e123a04262738d
SongkyoLee <songkyo.lee@cern.ch>
2016-03-17 01:15:56 +0900

    dimuon_pPb moved to other rep

--- Dimuons_pPb/MuonAnalysis/TagAndProbe/python/fakerate_all_cff.py
+++
@@ -1,8 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-from MuonAnalysis.TagAndProbe.fakerate_common_cff import *
-from MuonAnalysis.TagAndProbe.fakerate_jetPlusProbe_cff import *
-from MuonAnalysis.TagAndProbe.fakerate_wPlusProbe_cff import *
-from MuonAnalysis.TagAndProbe.fakerate_zPlusProbe_cff import *
-
-
