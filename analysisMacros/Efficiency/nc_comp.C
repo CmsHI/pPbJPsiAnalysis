b84048a46f84eb3db4dba258238d6eb2f17a7265
SongkyoLee <songkyo.lee@cern.ch>
2016-03-17 15:40:16 +0900

    efficiency comparison macro

---
+++ analysisMacros/Efficiency/nc_comp.C
@@ -0,0 +1,23 @@
+//void nc_totalComp()
+{
+	gStyle->SetOptStat(0);
+	gStyle->SetPaintTextFormat(".3f");
+	//gStyle->SetPaintTextFormat(".5f");
+	
+	TFile* f1 = TFile::Open("EffAna_8rap9pt_PRMCpythia_Pbp_Zvtx0_SF0_isOldAccCut0_isOfficial1.root");
+	//TFile* f2 = TFile::Open("AccAna_8rap9pt_pp_NP.root");
+	TFile* f2 = TFile::Open("EffAna_8rap9pt_Pbp_PR_newcut_Zvtx0_SF0.root");
+	//TFile* f2 = TFile::Open("rootNonOnia/AccAna_8rap9pt_PRMC_boosted.root");
+	
+	TCanvas* c1 = new TCanvas("c1","",600,600);
+	TH2D* h1 = (TH2D*)f1->Get("h2D_Eff_pt_y");
+	h1->SetName("h1");
+
+	TH2D* h2 = (TH2D*)f2->Get("h2D_Eff_pt_y");
+	//TH2D* h2 = (TH2D*)f2->Get("h2D_Acc_pt_y_Pbp");
+	h2->SetName("h2");
+	h1->Divide(h2);
+	h1->SetMaximum(1.3);	
+	h1->SetMaximum(1.7);	
+	h1->Draw("text e colz");
+}
--- analysisMacros/Efficiency_zVtx_embedded/kyo_run_vsZvtx.sh
+++
@@ -1,4 +0,0 @@
-root -l -q 'rootAna_efficiency_counting_vsZvtx.C+("8rap9pt2gev",1,1,0,0,0,0,0)'
-root -l -q 'rootAna_efficiency_counting_vsZvtx.C+("8rap9pt2gev",1,0,0,0,0,0,0)'
-root -l -q 'rootAna_efficiency_counting_vsZvtx.C+("8rap9pt2gev",0,1,0,0,0,0,0)'
-root -l -q 'rootAna_efficiency_counting_vsZvtx.C+("8rap9pt2gev",0,0,0,0,0,0,0)'
