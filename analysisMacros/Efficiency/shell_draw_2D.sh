ba5e2c92e0f6f9b1a0488b6385fbe7853ed603e5
SongkyoLee <songkyo.lee@cern.ch>
2016-05-13 15:57:53 +0900

    2D figures

--- analysisMacros/Efficiency/shell_draw_2D.sh
+++ analysisMacros/Efficiency/shell_draw_2D.sh
@@ -1,13 +1,13 @@
 ###void draw_2D_eff(int MrapNpt=89, int isPA = 1, int accCutType =2, bool isPrompt = false, bool useZvtxWeight=false, bool useSF=false, TString szDir="dir_eff")
 
 ### pp
-root -l -b -q 'draw_1D_eff.C+(89,0,2,true,false,false,"dir_eff")'
-root -l -b -q 'draw_1D_eff.C+(89,0,2,false,false,false,"dir_eff")'
-root -l -b -q 'draw_1D_eff_fine.C+(89,0,2,true,false,false,"dir_eff")'
-root -l -b -q 'draw_1D_eff_fine.C+(89,0,2,false,false,false,"dir_eff")'
+root -l -b -q 'draw_2D_eff.C+(89,0,2,true,false,false,"dir_eff")'
+root -l -b -q 'draw_2D_eff.C+(89,0,2,false,false,false,"dir_eff")'
+root -l -b -q 'draw_2D_eff_fine.C+(89,0,2,true,false,false,"dir_eff")'
+root -l -b -q 'draw_2D_eff_fine.C+(89,0,2,false,false,false,"dir_eff")'
 ### Pbp
-root -l -b -q 'draw_1D_eff.C+(89,1,2,true,false,false,"dir_eff")'
-root -l -b -q 'draw_1D_eff.C+(89,1,2,false,false,false,"dir_eff")'
-root -l -b -q 'draw_1D_eff_fine.C+(89,1,2,true,false,false,"dir_eff")'
-root -l -b -q 'draw_1D_eff_fine.C+(89,1,2,false,false,false,"dir_eff")'
+root -l -b -q 'draw_2D_eff.C+(89,1,2,true,false,false,"dir_eff")'
+root -l -b -q 'draw_2D_eff.C+(89,1,2,false,false,false,"dir_eff")'
+root -l -b -q 'draw_2D_eff_fine.C+(89,1,2,true,false,false,"dir_eff")'
+root -l -b -q 'draw_2D_eff_fine.C+(89,1,2,false,false,false,"dir_eff")'
 
