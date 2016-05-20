9a9d1eaa2a8775a8dc532c752613daf61d6fd12d
SongkyoLee <songkyo.lee@cern.ch>
2016-05-21 02:00:17 +0900

    graph style 2 added

--- analysisMacros/SONGKYO.h
+++ analysisMacros/SONGKYO.h
@@ -110,7 +110,7 @@ void SetGraphStyle(TGraph* gr, Int_t c, Int_t m) {
 }
 
 void SetGraphStyle2(TGraph* gr, Int_t c, Int_t m) {
-  Int_t colorArr[] = { kGray+3, kRed+2, kBlue+1, kOrange+7, kAzure+9, kViolet-4, kGreen+1, kBlack };
+  Int_t colorArr[] = { kGray+3, kRed+2, kBlue+1, kOrange+7, kAzure+9, kViolet-4, kGreen+1, kPink+9, kBlack };
   Int_t markerFullArr[] = {kFullCircle, kFullTriangleUp, kFullTriangleDown, kFullSquare, kFullStar, kFullDiamond};
   Int_t markerOpenArr[] = {kOpenCircle, kOpenTriangleUp, kOpenTriangleDown, kOpenSquare, kOpenStar, kOpenDiamond};
 
