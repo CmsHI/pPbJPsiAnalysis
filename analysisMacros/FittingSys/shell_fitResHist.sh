de095a6168a05f9c51d784429b84330c16e70593
SongkyoLee <songkyo.lee@cern.ch>
2016-05-20 17:27:02 +0900

    shell scripts for systemtacis

--- analysisMacros/FittingSys/shell_fitResHist.sh
+++ analysisMacros/FittingSys/shell_fitResHist.sh
@@ -1,9 +1,15 @@
-#int make_fitResHist(int MrapNpt=89, int isPA =3, int accCutType=1){
-root -l -b -q 'make_fitResHist.C+(89,0,1,"nominal")'
-root -l -b -q 'make_fitResHist.C+(89,0,2,"nominal")'
-#root -l -b -q 'make_fitResHist.C+(89,1,1,"nominal")'
-#root -l -b -q 'make_fitResHist.C+(89,1,2,"nominal")'
-#root -l -b -q 'make_fitResHist.C+(89,2,1,"nominal")'
-#root -l -b -q 'make_fitResHist.C+(89,2,2,"nominal")'
-root -l -b -q 'make_fitResHist.C+(89,3,1,"nominal")'
-root -l -b -q 'make_fitResHist.C+(89,3,2,"nominal")'
+#int make_fitResHist(int MrapNpt=89, int isPA =3, int accCutType=1, TString szSys="nominal"){
+
+#### pp
+#root -l -b -q 'make_fitResHist.C+(89,0,2,"sys01_01")'
+
+#### pA
+root -l -b -q 'make_fitResHist.C+(89,3,2,"sys01_01")'
+root -l -b -q 'make_fitResHist.C+(89,3,2,"sys01_02")'
+root -l -b -q 'make_fitResHist.C+(89,3,2,"sys01_03")'
+root -l -b -q 'make_fitResHist.C+(89,3,2,"sys01_04")'
+root -l -b -q 'make_fitResHist.C+(89,3,2,"sys01_05")'
+root -l -b -q 'make_fitResHist.C+(89,3,2,"sys02_01")'
+root -l -b -q 'make_fitResHist.C+(89,3,2,"sys03_01")'
+root -l -b -q 'make_fitResHist.C+(89,3,2,"sys03_02")'
+root -l -b -q 'make_fitResHist.C+(89,3,2,"sys04_01")'
