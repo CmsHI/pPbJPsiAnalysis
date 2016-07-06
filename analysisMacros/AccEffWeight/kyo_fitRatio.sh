3e440144de0a70ef14562657c5d57bf39d44e759
SongkyoLee <songkyo.lee@cern.ch>
2016-07-06 20:52:25 +0900

    fitting for pp also

--- analysisMacros/AccEffWeight/kyo_fitRatio.sh
+++
@@ -1,6 +0,0 @@
-## actual
-root -l 'fitRatio_DataMcRecoPt.C+(1,1,"8rap9pt")'
-root -l 'fitRatio_DataMcRecoPt.C+(1,0,"8rap9pt")'
-## not used
-root -l 'fitRatio_DataMcRecoPt.C+(0,1,"8rap9pt")'
-root -l 'fitRatio_DataMcRecoPt.C+(0,0,"8rap9pt")'
