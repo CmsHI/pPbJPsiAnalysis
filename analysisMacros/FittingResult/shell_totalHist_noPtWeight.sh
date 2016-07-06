aa237a1d99bd09c1855e07706a9bd72506d2f9f0
SongkyoLee <songkyo.lee@cern.ch>
2016-07-06 21:00:18 +0900

    recent result files

--- analysisMacros/FittingResult/shell_totalHist_noPtWeight.sh
+++ analysisMacros/FittingResult/shell_totalHist_noPtWeight.sh
@@ -3,13 +3,15 @@
 ########### new cut ##########
 #### pp
 #root -l -b -q 'make_totalHist_pp_noPtWeight.C+(89,2,false,false,"nominal")'
-root -l -b -q 'make_totalHist_pp_noPtWeight.C+(89,2,false,true,"nominal")'
-root -l -b -q 'make_totalHist_pp_noPtWeight_tag3.C+(89,2,false,true,"nominal")'
-root -l -b -q 'make_totalHist_pp_noPtWeight_eta45_tag5.C+(89,2,false,true,"nominal")'
+#root -l -b -q 'make_totalHist_pp_noPtWeight.C+(89,2,false,true,"nominal")'
+root -l -b -q 'make_totalHist_pp_noPtWeight_releasesigmaPRResW.C+(89,2,false,true,"nominal")'
+#root -l -b -q 'make_totalHist_pp_noPtWeight_tag3.C+(89,2,false,true,"nominal")'
+#root -l -b -q 'make_totalHist_pp_noPtWeight_eta45_tag5.C+(89,2,false,true,"nominal")'
 #### pA
 #root -l -b -q 'make_totalHist_pA_noPtWeight.C+(89,2,false,false,"nominal")'
 #root -l -b -q 'make_totalHist_pA_noPtWeight.C+(89,2,true,false,"nominal")'
-root -l -b -q 'make_totalHist_pA_noPtWeight.C+(89,2,true,true,"nominal")'
-root -l -b -q 'make_totalHist_pA_noPtWeight_tag3.C+(89,2,true,true,"nominal")'
-root -l -b -q 'make_totalHist_pA_noPtWeight_eta45_tag5.C+(89,2,true,true,"nominal")'
+#root -l -b -q 'make_totalHist_pA_noPtWeight.C+(89,2,true,true,"nominal")'
+root -l -b -q 'make_totalHist_pA_noPtWeight_releasesigmaPRResW.C+(89,2,true,true,"nominal")'
+#root -l -b -q 'make_totalHist_pA_noPtWeight_tag3.C+(89,2,true,true,"nominal")'
+#root -l -b -q 'make_totalHist_pA_noPtWeight_eta45_tag5.C+(89,2,true,true,"nominal")'
 
