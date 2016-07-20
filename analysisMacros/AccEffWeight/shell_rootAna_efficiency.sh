e593e76365e6cac6652c1b7436e4b5fee12008a3
SongkyoLee <songkyo.lee@cern.ch>
2016-07-20 18:19:05 +0900

    shell script only with needed options

--- analysisMacros/AccEffWeight/shell_rootAna_efficiency.sh
+++ analysisMacros/AccEffWeight/shell_rootAna_efficiency.sh
@@ -1,33 +1,19 @@
 #########	(MrapNpt, isPA, accCutType, isPrompt, useZvtxWeight,useSF)
 
-########## new cut
 ####### pp
-#root -l -b -q 'rootAna_efficiency_ptWeight.C+(89,0,2,true,false,false)'
-#root -l -b -q 'rootAna_efficiency_ptWeight.C+(89,0,2,true,false,true)'
-root -l -b -q 'rootAna_efficiency_ptWeight.C+(62,0,2,true,false,true)'
-#root -l -b -q 'rootAna_efficiency_ptWeight.C+(89,0,2,false,false,false)'
-#root -l -b -q 'rootAna_efficiency_ptWeight.C+(89,0,2,false,false,true)'
-root -l -b -q 'rootAna_efficiency_ptWeight.C+(62,0,2,false,false,true)'
+root -l -b -q 'rootAna_efficiency_ptWeight.C+(89,0,2,true,false,true)' #actual
+root -l -b -q 'rootAna_efficiency_ptWeight.C+(89,0,2,false,false,true)' #actual
 ####### Pbp
-#root -l -b -q 'rootAna_efficiency_ptWeight.C+(89,1,2,true,false,false)'
-#root -l -b -q 'rootAna_efficiency_ptWeight.C+(89,1,2,true,true,false)'
-#root -l -b -q 'rootAna_efficiency_ptWeight.C+(89,1,2,true,false,true)'
-#root -l -b -q 'rootAna_efficiency_ptWeight.C+(89,1,2,true,true,true)'
-root -l -b -q 'rootAna_efficiency_ptWeight.C+(62,1,2,true,true,true)'
-#root -l -b -q 'rootAna_efficiency_ptWeight.C+(89,1,2,false,false,false)'
-#root -l -b -q 'rootAna_efficiency_ptWeight.C+(89,1,2,false,true,false)'
-#root -l -b -q 'rootAna_efficiency_ptWeight.C+(89,1,2,false,false,true)'
-#root -l -b -q 'rootAna_efficiency_ptWeight.C+(89,1,2,false,true,true)'
-root -l -b -q 'rootAna_efficiency_ptWeight.C+(62,1,2,false,true,true)'
+root -l -b -q 'rootAna_efficiency_ptWeight.C+(89,1,2,true,true,true)' #actual
+root -l -b -q 'rootAna_efficiency_ptWeight.C+(89,1,2,false,true,true)' #actual
 ####### pPb
-#root -l -b -q 'rootAna_efficiency_ptWeight.C+(89,2,2,true,false,false)'
-#root -l -b -q 'rootAna_efficiency_ptWeight.C+(89,2,2,true,true,false)'
-#root -l -b -q 'rootAna_efficiency_ptWeight.C+(89,2,2,true,false,true)'
-#root -l -b -q 'rootAna_efficiency_ptWeight.C+(89,2,2,true,true,true)'
-root -l -b -q 'rootAna_efficiency_ptWeight.C+(62,2,2,true,true,true)'
-#root -l -b -q 'rootAna_efficiency_ptWeight.C+(89,2,2,false,false,false)'
-#root -l -b -q 'rootAna_efficiency_ptWeight.C+(89,2,2,false,true,false)'
-#root -l -b -q 'rootAna_efficiency_ptWeight.C+(89,2,2,false,false,true)'
-#root -l -b -q 'rootAna_efficiency_ptWeight.C+(89,2,2,false,true,true)'
-root -l -b -q 'rootAna_efficiency_ptWeight.C+(62,2,2,false,true,true)'
+root -l -b -q 'rootAna_efficiency_ptWeight.C+(89,2,2,true,true,true)' #actual
+root -l -b -q 'rootAna_efficiency_ptWeight.C+(89,2,2,false,true,true)' #actual
 
+### for ET dependence
+####### Pbp
+root -l -b -q 'rootAna_efficiency_ptWeight.C+(62,1,2,true,true,true)' #actual
+root -l -b -q 'rootAna_efficiency_ptWeight.C+(62,1,2,false,true,true)' #actual
+####### pPb
+root -l -b -q 'rootAna_efficiency_ptWeight.C+(62,2,2,true,true,true)' #actual
+root -l -b -q 'rootAna_efficiency_ptWeight.C+(62,2,2,false,true,true)' #actual
