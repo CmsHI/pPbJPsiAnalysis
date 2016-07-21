e57aacc25ad18b33a8b0596cc316893eaa118c67
JaeBeom Park <pjwinnetou@gmail.com>
2016-07-21 15:13:38 +0900

    .

--- analysisMacros/AccEffWeight/nc_comp_2D.C
+++ analysisMacros/AccEffWeight/nc_comp_2D.C
@@ -3,13 +3,13 @@
 {
 	gStyle->SetPaintTextFormat(".5f");
 
-	//TFile *f1 = TFile::Open("AccAna_8rap9pt_pp_PR_newcut_ptWeight.root");
+	TFile *f1 = TFile::Open("AccAna_8rap9pt_pp_PR_newcut_ptWeight.root");
 	//TFile *f1 = TFile::Open("AccAna_8rap9pt_pp_NP_newcut_ptWeight.root");
 	//TFile *f1 = TFile::Open("AccAna_8rap9pt_pA_PR_newcut_ptWeight.root");
 	//TFile *f1 = TFile::Open("AccAna_8rap9pt_pA_NP_newcut_ptWeight.root");
 	
   //TFile *f1 = TFile::Open("EffAna_8rap9pt_pp_PR_newcut_Zvtx0_SF1.root");
-	TFile *f1 = TFile::Open("EffAna_8rap9pt_pp_NP_newcut_Zvtx0_SF1.root");
+	//TFile *f1 = TFile::Open("EffAna_8rap9pt_pp_NP_newcut_Zvtx0_SF1.root");
 	//TFile *f1 = TFile::Open("EffAna_8rap9pt_Pbp_PR_newcut_Zvtx1_SF1.root");
 	//TFile *f1 = TFile::Open("EffAna_8rap9pt_Pbp_NP_newcut_Zvtx1_SF1.root");
 	//TFile *f1 = TFile::Open("EffAna_8rap9pt_pPb_PR_newcut_Zvtx1_SF1.root");
@@ -17,14 +17,14 @@
 
 	TCanvas* c1 = new TCanvas("c1","",600,600);
 
-	//TH2D* h1 = (TH2D*)f1->Get("h2D_Acc_pt_y");
+	TH2D* h1 = (TH2D*)f1->Get("h2D_Acc_pt_y");
 	//TH2D* h1 = (TH2D*)f1->Get("h2D_Acc_pt_y_Pbp");
-	TH2D* h1 = (TH2D*)f1->Get("h2D_Eff_pt_y");
+	//TH2D* h1 = (TH2D*)f1->Get("h2D_Eff_pt_y");
 	h1->SetName("h1");
 
-	//TH2D* h2 = (TH2D*)f1->Get("h2D_Acc_pt_y_noPtWeight");
+	TH2D* h2 = (TH2D*)f1->Get("h2D_Acc_pt_y_noPtWeight");
 	//TH2D* h2 = (TH2D*)f1->Get("h2D_Acc_pt_y_noPtWeight_Pbp");
-	TH2D* h2 = (TH2D*)f1->Get("h2D_Eff_pt_y_noPtWeight");
+	//TH2D* h2 = (TH2D*)f1->Get("h2D_Eff_pt_y_noPtWeight");
 	h2->SetName("h2");
 	h1->Divide(h2);
 	h1->Draw("text e colz");
--- analysisMacros/AccEffWeight/shell_rootAna_efficiency.sh
+++ analysisMacros/AccEffWeight/shell_rootAna_efficiency.sh
@@ -1,19 +1,19 @@
 #########	(MrapNpt, isPA, accCutType, isPrompt, useZvtxWeight,useSF)
 
 ####### pp
-root -l -b -q 'rootAna_efficiency_ptWeight.C+(89,0,2,true,false,true)' #actual
+#root -l -b -q 'rootAna_efficiency_ptWeight.C+(89,0,2,true,false,true)' #actual
 root -l -b -q 'rootAna_efficiency_ptWeight.C+(89,0,2,false,false,true)' #actual
 ####### Pbp
-root -l -b -q 'rootAna_efficiency_ptWeight.C+(89,1,2,true,true,true)' #actual
-root -l -b -q 'rootAna_efficiency_ptWeight.C+(89,1,2,false,true,true)' #actual
+#root -l -b -q 'rootAna_efficiency_ptWeight.C+(89,1,2,true,true,true)' #actual
+#root -l -b -q 'rootAna_efficiency_ptWeight.C+(89,1,2,false,true,true)' #actual
 ####### pPb
-root -l -b -q 'rootAna_efficiency_ptWeight.C+(89,2,2,true,true,true)' #actual
-root -l -b -q 'rootAna_efficiency_ptWeight.C+(89,2,2,false,true,true)' #actual
+#root -l -b -q 'rootAna_efficiency_ptWeight.C+(89,2,2,true,true,true)' #actual
+#root -l -b -q 'rootAna_efficiency_ptWeight.C+(89,2,2,false,true,true)' #actual
 
 ### for ET dependence
 ####### Pbp
-root -l -b -q 'rootAna_efficiency_ptWeight.C+(62,1,2,true,true,true)' #actual
-root -l -b -q 'rootAna_efficiency_ptWeight.C+(62,1,2,false,true,true)' #actual
+#root -l -b -q 'rootAna_efficiency_ptWeight.C+(62,1,2,true,true,true)' #actual
+#root -l -b -q 'rootAna_efficiency_ptWeight.C+(62,1,2,false,true,true)' #actual
 ####### pPb
-root -l -b -q 'rootAna_efficiency_ptWeight.C+(62,2,2,true,true,true)' #actual
-root -l -b -q 'rootAna_efficiency_ptWeight.C+(62,2,2,false,true,true)' #actual
+#root -l -b -q 'rootAna_efficiency_ptWeight.C+(62,2,2,true,true,true)' #actual
+#root -l -b -q 'rootAna_efficiency_ptWeight.C+(62,2,2,false,true,true)' #actual
