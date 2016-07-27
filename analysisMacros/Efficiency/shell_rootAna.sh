cc40e9b205db6665c3b24096dd12c10e90745a4d
SongkyoLee <songkyo.lee@cern.ch>
2016-07-27 19:18:47 +0900

    new SF for pp (0727)

--- analysisMacros/Efficiency/shell_rootAna.sh
+++ analysisMacros/Efficiency/shell_rootAna.sh
@@ -1,77 +1,33 @@
 #########	(MrapNpt, isPA, accCutType, isPrompt, useZvtxWeight,useSF)
 
-########## old cut
-####### pp
-#root -l -b -q 'rootAna_efficiency.C+(89,0,1,true,false,false)'
-#root -l -b -q 'rootAna_efficiency.C+(89,0,1,true,true,false)'
-#root -l -b -q 'rootAna_efficiency.C+(89,0,1,true,false,true)'
-#root -l -b -q 'rootAna_efficiency.C+(89,0,1,true,true,true)'
-#root -l -b -q 'rootAna_efficiency.C+(89,0,1,false,false,false)'
-#root -l -b -q 'rootAna_efficiency.C+(89,0,1,false,true,false)'
-#root -l -b -q 'rootAna_efficiency.C+(89,0,1,false,false,true)'
-#root -l -b -q 'rootAna_efficiency.C+(89,0,1,false,true,true)'
-####### Pbp
-#root -l -b -q 'rootAna_efficiency.C+(89,1,1,true,false,false)'
-#root -l -b -q 'rootAna_efficiency.C+(89,1,1,true,true,false)'
-#root -l -b -q 'rootAna_efficiency.C+(89,1,1,true,false,true)'
-#root -l -b -q 'rootAna_efficiency.C+(89,1,1,true,true,true)'
-#root -l -b -q 'rootAna_efficiency.C+(89,1,1,false,false,false)'
-#root -l -b -q 'rootAna_efficiency.C+(89,1,1,false,true,false)'
-#root -l -b -q 'rootAna_efficiency.C+(89,1,1,false,false,true)'
-#root -l -b -q 'rootAna_efficiency.C+(89,1,1,false,true,true)'
-####### pPb
-#root -l -b -q 'rootAna_efficiency.C+(89,2,1,true,false,false)'
-#root -l -b -q 'rootAna_efficiency.C+(89,2,1,true,true,false)'
-#root -l -b -q 'rootAna_efficiency.C+(89,2,1,true,false,true)'
-#root -l -b -q 'rootAna_efficiency.C+(89,2,1,true,true,true)'
-#root -l -b -q 'rootAna_efficiency.C+(89,2,1,false,false,false)'
-#root -l -b -q 'rootAna_efficiency.C+(89,2,1,false,true,false)'
-#root -l -b -q 'rootAna_efficiency.C+(89,2,1,false,false,true)'
-#root -l -b -q 'rootAna_efficiency.C+(89,2,1,false,true,true)'
-
-
 ########## new cut
 ####### pp
 #root -l -b -q 'rootAna_efficiency.C+(89,0,2,true,false,false)'
-#root -l -b -q 'rootAna_efficiency.C+(89,0,2,true,false,true)'
-root -l -b -q 'rootAna_efficiency.C+(89,0,2,true,true,true)'
-#root -l -b -q 'rootAna_efficiency.C+(62,0,2,true,false,true)'
-#root -l -b -q 'rootAna_efficiency_tag3.C+(89,0,2,true,false,true)'
-#root -l -b -q 'rootAna_efficiency_eta45_tag5.C+(89,0,2,true,false,true)'
+root -l -b -q 'rootAna_efficiency.C+(89,0,2,true,false,true)' ## actual
+#root -l -b -q 'rootAna_efficiency.C+(89,0,2,true,true,true)'
 #root -l -b -q 'rootAna_efficiency.C+(89,0,2,false,false,false)'
-#root -l -b -q 'rootAna_efficiency.C+(89,0,2,false,false,true)'
-root -l -b -q 'rootAna_efficiency.C+(89,0,2,false,true,true)'
-#root -l -b -q 'rootAna_efficiency.C+(62,0,2,false,false,true)'
-#root -l -b -q 'rootAna_efficiency_tag3.C+(89,0,2,false,false,true)'
-#root -l -b -q 'rootAna_efficiency_eta45_tag5.C+(89,0,2,false,false,true)'
+root -l -b -q 'rootAna_efficiency.C+(89,0,2,false,false,true)' ## actual
+#root -l -b -q 'rootAna_efficiency.C+(89,0,2,false,true,true)'
 ####### Pbp
 #root -l -b -q 'rootAna_efficiency.C+(89,1,2,true,false,false)'
 #root -l -b -q 'rootAna_efficiency.C+(89,1,2,true,true,false)'
 #root -l -b -q 'rootAna_efficiency.C+(89,1,2,true,false,true)'
-#root -l -b -q 'rootAna_efficiency.C+(89,1,2,true,true,true)'
-#root -l -b -q 'rootAna_efficiency.C+(62,1,2,true,true,true)'
-#root -l -b -q 'rootAna_efficiency_tag3.C+(89,1,2,true,true,true)'
-#root -l -b -q 'rootAna_efficiency_eta45_tag5.C+(89,1,2,true,true,true)'
+#root -l -b -q 'rootAna_efficiency.C+(89,1,2,true,true,true)' ##actual
+#root -l -b -q 'rootAna_efficiency.C+(62,1,2,true,true,true)' ##actual ET
 #root -l -b -q 'rootAna_efficiency.C+(89,1,2,false,false,false)'
 #root -l -b -q 'rootAna_efficiency.C+(89,1,2,false,true,false)'
 #root -l -b -q 'rootAna_efficiency.C+(89,1,2,false,false,true)'
-#root -l -b -q 'rootAna_efficiency.C+(89,1,2,false,true,true)'
-#root -l -b -q 'rootAna_efficiency.C+(62,1,2,false,true,true)'
-#root -l -b -q 'rootAna_efficiency_tag3.C+(89,1,2,false,true,true)'
-#root -l -b -q 'rootAna_efficiency_eta45_tag5.C+(89,1,2,false,true,true)'
+#root -l -b -q 'rootAna_efficiency.C+(89,1,2,false,true,true)' ## actual
+#root -l -b -q 'rootAna_efficiency.C+(62,1,2,false,true,true)' ##actual ET
 ####### pPb
 #root -l -b -q 'rootAna_efficiency.C+(89,2,2,true,false,false)'
 #root -l -b -q 'rootAna_efficiency.C+(89,2,2,true,true,false)'
 #root -l -b -q 'rootAna_efficiency.C+(89,2,2,true,false,true)'
-#root -l -b -q 'rootAna_efficiency.C+(89,2,2,true,true,true)'
-#root -l -b -q 'rootAna_efficiency.C+(62,2,2,true,true,true)'
-#root -l -b -q 'rootAna_efficiency_tag3.C+(89,2,2,true,true,true)'
-#root -l -b -q 'rootAna_efficiency_eta45_tag5.C+(89,2,2,true,true,true)'
+#root -l -b -q 'rootAna_efficiency.C+(89,2,2,true,true,true)' ## actual
+#root -l -b -q 'rootAna_efficiency.C+(62,2,2,true,true,true)' ## actual ET
 #root -l -b -q 'rootAna_efficiency.C+(89,2,2,false,false,false)'
 #root -l -b -q 'rootAna_efficiency.C+(89,2,2,false,true,false)'
 #root -l -b -q 'rootAna_efficiency.C+(89,2,2,false,false,true)'
-#root -l -b -q 'rootAna_efficiency.C+(89,2,2,false,true,true)'
-#root -l -b -q 'rootAna_efficiency.C+(62,2,2,false,true,true)'
-#root -l -b -q 'rootAna_efficiency_tag3.C+(89,2,2,false,true,true)'
-#root -l -b -q 'rootAna_efficiency_eta45_tag5.C+(89,2,2,false,true,true)'
+#root -l -b -q 'rootAna_efficiency.C+(89,2,2,false,true,true)' ## actual
+#root -l -b -q 'rootAna_efficiency.C+(62,2,2,false,true,true)' ## actual ET
 
