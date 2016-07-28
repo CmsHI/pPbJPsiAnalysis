33875cbe718e1a1ef5c22deefc3f9de446b05af8
SongkyoLee <songkyo.lee@cern.ch>
2016-07-28 13:18:43 +0900

    comparison plot

--- analysisMacros/TheoryPrediction/shell_runall.sh
+++ analysisMacros/TheoryPrediction/shell_runall.sh
@@ -1,4 +1,4 @@
-#root -l 'comp_RFB_rap_Vogt.C+(true, false)'
+root -l 'comp_RFB_rap_Vogt.C+(true, false)'
 root -l 'comp_RFB_pt_Vogt.C+(20,true, false)'
 
 #root -l 'comp_RFB_rap_Vogt.C+(false, false)'
