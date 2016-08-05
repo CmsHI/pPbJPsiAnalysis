4e8476ae553edbfd63cf5854b8d061835f07c857
SongkyoLee <songkyo.lee@cern.ch>
2016-08-05 15:13:39 +0900

    old single muon acc cuts removed

---
+++ analysisMacros/Acceptance/dir_acc/README.txt
@@ -0,0 +1,1 @@
+Here acceptance plots is stored
--- analysisMacros/Acceptance/shell_rootAna.sh
+++ analysisMacros/Acceptance/shell_rootAna.sh
@@ -1,28 +1,10 @@
 #int rootAna_acceptance(int MrapNpt=89, int isPA = 0, int accCutType =2, bool isPrompt=true){
-
-##### old cut
 ### pp
-#root -l -b -q 'rootAna_acceptance.C+(89,0,1,true)'
-#root -l -b -q 'rootAna_acceptance.C+(89,0,1,false)'
-root -l -b -q 'rootAna_acceptance.C+(62,0,1,true)'
-root -l -b -q 'rootAna_acceptance.C+(62,0,1,false)'
+root -l -b -q 'rootAna_acceptance.C+(89,0,2,true)'
+root -l -b -q 'rootAna_acceptance.C+(89,0,2,false)'
 ### Pbp
-#root -l -b -q 'rootAna_acceptance.C+(89,1,1,true)'
-#root -l -b -q 'rootAna_acceptance.C+(89,1,1,false)'
-root -l -b -q 'rootAna_acceptance.C+(62,1,1,true)'
-root -l -b -q 'rootAna_acceptance.C+(62,1,1,false)'
-
-
-
-##### new cut
-### pp
-#root -l -b -q 'rootAna_acceptance.C+(89,0,2,true)'
-#root -l -b -q 'rootAna_acceptance.C+(89,0,2,false)'
-root -l -b -q 'rootAna_acceptance.C+(62,0,2,true)'
-root -l -b -q 'rootAna_acceptance.C+(62,0,2,false)'
-### Pbp
-#root -l -b -q 'rootAna_acceptance.C+(89,1,2,true)'
-#root -l -b -q 'rootAna_acceptance.C+(89,1,2,false)'
+root -l -b -q 'rootAna_acceptance.C+(89,1,2,true)'
+root -l -b -q 'rootAna_acceptance.C+(89,1,2,false)'
 root -l -b -q 'rootAna_acceptance.C+(62,1,2,true)'
 root -l -b -q 'rootAna_acceptance.C+(62,1,2,false)'
 
