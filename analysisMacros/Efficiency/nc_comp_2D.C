240176fb91d6f2cca6d40c42c3c1192cd5a6e7c1
SongkyoLee <songkyo.lee@cern.ch>
2016-08-05 13:29:47 +0900

    change the plot style

--- analysisMacros/Efficiency/nc_comp_2D.C
+++ analysisMacros/Efficiency/nc_comp_2D.C
@@ -8,10 +8,11 @@
   //TFile* f02 = TFile::Open("EffAna_8rap9pt_pp_PR_newcut_Zvtx1_SF0.root");
   //TFile* f01 = TFile::Open("EffAna_8rap9pt_pp_PR_newcut_Zvtx1_SF0_test02.root");
   //TFile* f02 = TFile::Open("EffAna_8rap9pt_pp_PR_newcut_Zvtx0_SF0.root");
-  //TFile* f01 = TFile::Open("EffAna_8rap9pt_Pbp_PR_newcut_Zvtx1_SF1.root");
+  TFile* f01 = TFile::Open("EffAna_8rap9pt_Pbp_PR_newcut_Zvtx1_SF1.root");
+  TFile* f02 = TFile::Open("EffAna_8rap9pt_Pbp_PR_newcut_Zvtx1_SF1_testCtErr.root");
   //TFile* f02 = TFile::Open("OldCutTest/EffAna_8rap9pt_Pbp_PR_newcut_Zvtx1_SF1.root");
-  TFile* f01 = TFile::Open("EffAna_8rap9pt_pPb_PR_newcut_Zvtx1_SF1.root");
-  TFile* f02 = TFile::Open("OldCutTest/EffAna_8rap9pt_pPb_PR_newcut_Zvtx1_SF1.root");
+  //TFile* f01 = TFile::Open("EffAna_8rap9pt_pPb_PR_newcut_Zvtx1_SF1.root");
+  //TFile* f02 = TFile::Open("OldCutTest/EffAna_8rap9pt_pPb_PR_newcut_Zvtx1_SF1.root");
   
   TCanvas* c1 = new TCanvas("c1","",600,600);
   TH2D* h2D_01 = (TH2D*)f01->Get("h2D_Eff_pt_y");
