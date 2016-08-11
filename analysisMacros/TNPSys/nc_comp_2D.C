12409fd7499a589bba76682e73132ea8bf4b6cff
SongkyoLee <songkyo.lee@cern.ch>
2016-08-11 17:08:25 +0900

    new pp TNP sys (v9 nomu7 0810)

--- analysisMacros/TNPSys/nc_comp_2D.C
+++ analysisMacros/TNPSys/nc_comp_2D.C
@@ -4,8 +4,10 @@
   //gStyle->SetPaintTextFormat(".3f");
   gStyle->SetPaintTextFormat(".5f");
   
-  TFile* f01 = TFile::Open("Total_TnP_sys_8rap9pt_pp_PR.root");
-  TFile* f02 = TFile::Open("../TNPSys_orig/Total_TnP_sys_8rap9pt_pp_PR.root");
+  TFile* f01 = TFile::Open("Total_TnP_sys_8rap9pt_pp_NP.root");
+  TFile* f02 = TFile::Open("Total_TnP_sys_8rap9pt_pp_NP_orig.root");
+  //TFile* f01 = TFile::Open("Total_TnP_sys_6rap2pt_Pbp_PR.root");
+  //TFile* f02 = TFile::Open("../TNPSys_orig/Total_TnP_sys_6rap2pt_Pbp_PR.root");
   
   TCanvas* c1 = new TCanvas("c1","",600,600);
   TH2D* h2D_01 = (TH2D*)f01->Get("htot");
