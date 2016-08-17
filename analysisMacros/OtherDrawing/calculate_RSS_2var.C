72ecc5adc7254deef7019960439c22aa531ca90a
SongkyoLee <songkyo.lee@cern.ch>
2016-08-17 20:17:50 +0900

    quick check for RSS

---
+++ analysisMacros/OtherDrawing/calculate_RSS_2var.C
@@ -0,0 +1,3 @@
+double calculate_RSS_2var(double x1,double x2){
+  return sqrt(x1*x1+x2*x2);
+}
---
+++ analysisMacros/OtherDrawing/calculate_RSS_3var.C
@@ -0,0 +1,3 @@
+double calculate_RSS_3var(double x1,double x2,double x3){
+  return sqrt(x1*x1+x2*x2+x3*x3);
+}
