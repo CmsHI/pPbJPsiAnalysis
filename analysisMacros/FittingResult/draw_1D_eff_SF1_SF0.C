8caaf3cf84b8cd124df5e301eb3d7b0bb140406e
SongkyoLee <songkyo.lee@cern.ch>
2016-08-19 17:30:07 +0900

    latex location changed

--- analysisMacros/FittingResult/draw_1D_eff_SF1_SF0.C
+++ analysisMacros/FittingResult/draw_1D_eff_SF1_SF0.C
@@ -206,8 +206,10 @@ void draw_1D_eff_SF1_SF0(TString szBinning="8rap9pt", int isPA=0, bool isPrompt=
 			legBR -> AddEntry(g_01[iy],"SF","lp");
 			legBR -> AddEntry(g_02[iy],"no SF","lp");
 			legBR->Draw();
-			if (isPrompt) { latex->DrawLatex(0.19,0.23,Form("%s Prompt J/#psi",szPA.Data())); }
-			else { latex->DrawLatex(0.19,0.23,Form("%s Non-prompt J/#psi",szPA.Data())); }
+			//if (isPrompt) { latex->DrawLatex(0.19,0.23,Form("%s Prompt J/#psi",szPA.Data())); }
+			//else { latex->DrawLatex(0.19,0.23,Form("%s Non-prompt J/#psi",szPA.Data())); }
+			if (isPrompt) { latex->DrawLatex(0.55,0.85,Form("%s Prompt J/#psi",szPA.Data())); }
+			else { latex->DrawLatex(0.55,0.85,Form("%s Non-prompt J/#psi",szPA.Data())); }
 		}
 		latex->DrawLatex(0.55,0.23,Form("%s",rapArr[iy].Data()));
 	}
