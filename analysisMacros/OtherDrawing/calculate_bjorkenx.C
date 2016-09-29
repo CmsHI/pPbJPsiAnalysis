3d0d5906634c1800285b3440505e8cfb634c0e13
SongkyoLee <songkyo.lee@cern.ch>
2016-09-29 23:07:11 +0900

    bjorken x range max and middle in the bin

--- analysisMacros/OtherDrawing/calculate_bjorkenx.C
+++ analysisMacros/OtherDrawing/calculate_bjorkenx.C
@@ -14,8 +14,17 @@ void calculate_bjorkenx(Int_t isLHC=1,Int_t useQ = 0, Double_t q_square=13)
   // every units in GeV!!
   /////////////////////////
   
-  Double_t ptArr[] = {2.5,17}; 
-  Double_t rapArr[] = { -2.87, 1.93};
+  //Double_t ptArr[] = {2.5,17}; 
+  //Double_t ptArr[] = {2,2.5,17,30}; 
+  //Double_t ptArr[] = {5, 5.75, 6.5, 7, 17, 30}; 
+  //Double_t ptArr[] = {7, 17}; 
+  Double_t ptArr[] = {0.}; 
+  //Double_t rapArr[] = { -2.87, 1.93};
+  //Double_t rapArr[] = { -2.165};
+  Double_t rapArr[] = {2.4};
+  //Double_t rapArr[] = { -1.93, 1.93};
+  //Double_t rapArr[] = { -2.4, 1.93};
+  //Double_t rapArr[] = {-0.45,0.45};
   const Int_t nPtBins = sizeof(ptArr)/sizeof(double);
   const Int_t nYBins = sizeof(rapArr)/sizeof(double);
   cout << "nPtBins=" << nPtBins << endl;
@@ -50,11 +59,13 @@ void calculate_bjorkenx(Int_t isLHC=1,Int_t useQ = 0, Double_t q_square=13)
     if (useQ==0) {
       for (Int_t ipt=0; ipt < nPtBins; ipt++) {
         bjorkenX1[ir][ipt] = (1/sqrt_sNN)*TMath::Sqrt(jpsiM*jpsiM + ptArr[ipt]*ptArr[ipt])*TMath::Exp(-rapArr[ir]);
-      cout << "In given pT = " <<ptArr[ipt] <<" (GeV/c), and rapidity y =" << rapArr[ir] << endl;
+      //cout << "In given pT = " <<ptArr[ipt] <<" (GeV/c), and rapidity y =" << rapArr[ir] << endl;
+      //cout << " Q = sqrt( m^2 + pT^2 ) = " <<TMath::Sqrt(jpsiM*jpsiM + ptArr[ipt]*ptArr[ipt]) << endl;
+      //cout << "Bjorken x = " << bjorkenX1[ir][ipt] << endl;
+      cout << "y = "<<rapArr[ir]<< ", pT ="<<ptArr[ipt]<<" :: Bjorken x = " << bjorkenX1[ir][ipt] << endl;
       cout << " Q = sqrt( m^2 + pT^2 ) = " <<TMath::Sqrt(jpsiM*jpsiM + ptArr[ipt]*ptArr[ipt]) << endl;
-      cout << "Bjorken x = " << bjorkenX1[ir][ipt] << endl;
-      cout << endl;
       }
+      cout << endl;
     } else if (useQ==1 || useQ ==2) {
         bjorkenX2[ir] = (1/sqrt_sNN)*sqrt_Q*TMath::Exp(-rapArr[ir]);
         cout << "In given sqrt_Q = " <<sqrt_Q <<" (GeV/c), and rapidity y =" << rapArr[ir] << endl;
